e401cf8536807b98d07ca89dd91f6d704a7b5382
Muzaffarzoda <muzaffarzoda@gmail.com>
2023-06-18 21:03:15 -0400

    day o1

--- src/main/java/com/cydeo/config/SecurityConfig.java
+++ src/main/java/com/cydeo/config/SecurityConfig.java
@@ -18,29 +18,32 @@ import java.util.List;
 @Configuration
 public class SecurityConfig {
 
-    @Bean
-    public UserDetailsService userDetailsService(PasswordEncoder encoder){
-
-        List<UserDetails> userList = new ArrayList<>();
-
-        userList.add(
-                new User("mike", encoder.encode( "password"), Arrays.asList(new SimpleGrantedAuthority("ROLE_ADMIN"))));
-
-        userList.add(
-                new User("Abdu", encoder.encode( "password"), Arrays.asList(new SimpleGrantedAuthority("ROLE_MANAGER"))));
-
-        return new InMemoryUserDetailsManager(userList);
-    }
+//    @Bean
+//    public UserDetailsService userDetailsService(PasswordEncoder encoder){
+//
+////        List<UserDetails> userList = new ArrayList<>();
+////
+////        userList.add(
+////                new User("mike", encoder.encode( "password"), Arrays.asList(new SimpleGrantedAuthority("ROLE_ADMIN"))));
+////
+////        userList.add(
+////                new User("Abdu", encoder.encode( "password"), Arrays.asList(new SimpleGrantedAuthority("ROLE_MANAGER"))));
+////
+////        return new InMemoryUserDetailsManager(userList);
+////    }
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 
         return http
                 .authorizeRequests()
-                .antMatchers("/user/**").hasRole("ADMIN")
-                .antMatchers("/project/**").hasRole("MANAGER")
-                .antMatchers("/task/employee/**").hasRole("EMPLOYEE")
-                .antMatchers("/task/**").hasRole("MANAGER")
+//                .antMatchers("/user/**").hasRole("ADMIN")
+                .antMatchers("/user/**").hasAuthority("ROLE_ADMIN")
+//                .antMatchers("/project/**").hasRole("MANAGER")
+//                .antMatchers("/task/employee/**").hasRole("EMPLOYEE")
+//                .antMatchers("/task/**").hasRole("MANAGER")
+//                .antMatchers("/task/**").hasAnyRole("EMPLOYEE", "ADMIN")
+//                .antMatchers("/task/**").hasAuthority("ROLE_EMPLOYEE")
                 .antMatchers(
                         "/",
                         "/login",
@@ -50,7 +53,12 @@ public class SecurityConfig {
                 ).permitAll()
                 .anyRequest().authenticated()
                 .and()
-                .httpBasic()
+//                .httpBasic()
+                .formLogin()
+                .loginPage("/login")
+                .defaultSuccessUrl("/welcome")
+                .failureUrl("/login?error=true")
+                .permitAll()
                 .and().build();
     }
 
